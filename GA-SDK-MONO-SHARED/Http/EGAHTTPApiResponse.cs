66fb9aa6f809b7b62af5b1fd17c7dafb05d42412
lmagitem <lucmagitem@gmail.com>
2025-03-16 14:32:11 +0100

    Progress on Analytics

--- GA-SDK-MONO-SHARED/Http/EGAHTTPApiResponse.cs
+++ GA-SDK-MONO-SHARED/Http/EGAHTTPApiResponse.cs
@@ -1,19 +1,18 @@
-﻿namespace GameAnalyticsSDK.Net.Http
-{
-    internal enum EGAHTTPApiResponse
-    {
-        // client
-        NoResponse,
-        BadResponse,
-        RequestTimeout, // 408
-        JsonEncodeFailed,
-        JsonDecodeFailed,
-        // server
-        InternalServerError,
-        BadRequest, // 400
-        Unauthorized, // 401
-        UnknownResponseCode,
-        Ok,
-        Created
-    }
+﻿namespace GameAnalyticsSDK.Net.Http {
+  public enum EGAHTTPApiResponse {
+    // client
+    NoResponse,
+    BadResponse,
+    RequestTimeout, // 408
+    JsonEncodeFailed,
+    JsonDecodeFailed,
+
+    // server
+    InternalServerError,
+    BadRequest,   // 400
+    Unauthorized, // 401
+    UnknownResponseCode,
+    Ok,
+    Created
+  }
 }
